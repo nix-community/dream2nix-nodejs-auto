6d1fdefd1d6a4b52a23c9232a1cbf9084968ee31
DavHau <hsngrmpf+github@gmail.com>
2022-09-01 11:12:54 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Dream2nix auto-generated nodejs packages";
+
+  inputs = {
+    dream2nix.url = "github:nix-community/dream2nix/fix-index-apps";
+  };
+
+  outputs = inp:
+    (inp.dream2nix.lib.makeFlakeOutputsForIndexes {
+      source = ./.;
+      systems = ["x86_64-linux"];
+      indexes = {
+        libraries-io = {
+          platform = "npm";
+          number = 100;
+        };
+      };
+      packageOverrides = {
+        "^.*$".disable-build = {
+          buildScript = ":";
+        };
+      };
+    })
+    // {
+      checks = inp.self.packages;
+    };
+}
